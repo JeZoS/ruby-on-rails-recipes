f33d6e29492eec9d303f4db526ada41b6d936420
JeZoS <prabhat262111@gmail.com>
2021-05-21 23:36:49 +0530

    comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+    def create
+        @recipe = Recipe.find(params[:recipe_id])
+        @comment = @recipe.comments.build(comment_params)
+        @comment.chef = current_chef
+        if @comment.save
+            flash[:success] = 'Comment created successfully'
+            redirect_to recipe_path(@recipe)
+        else
+            flash[:danger] = 'Comment was not created'
+            redirect_to recipe_path(@recipe)
+        end
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:description)
+    end
+end
